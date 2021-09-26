ae0bede20bd36bbfd557d67de7ec5118ec913a80
Alexandr <apower.ru@mail.ru>
2021-09-26 20:43:31 +0300

    input by digit

--- notes.py
+++ notes.py
@@ -1,4 +1,4 @@
-def add_note_text(title):
+def input_note_text(title):
     add_note_text = '*'
     while add_note_text != '':
         add_note_text = input('Добавить (или "enter" для выхода): ')
@@ -8,15 +8,15 @@ def add_note_text(title):
 
 with open('D:\\notes.txt', 'r', encoding='cp1251') as inf:
     text_notes_in_memory = inf.read().strip().split('\n')
-    text_notes = {}
+    text_notes, ident = {}, {}
     for string in text_notes_in_memory:
         string = (string.strip().split('***'))
-        text_notes[string[0]] = string[1]
-    for i, note_title in enumerate(text_notes):
-        print(i + 1, '-', note_title)
+        ident[string[0]] = string[1]
+        text_notes[string[1]] = string[2]
+        print(string[0], '-', string[1])
     my_request = input('Введите номер нужной записи или "enter" для новой записи: ')
-    if my_request in text_notes.keys():
-        print('ОТВЕТ: ', *text_notes[my_request].split('___'), sep='\n- ')
+    if my_request in ident.keys():
+        print('ОТВЕТ: ', *text_notes[ident[my_request]].split('___'), sep='\n- ')
     elif my_request == '':
         note_title = input('Название заметки: ').lower()
         if note_title != '':
@@ -25,15 +25,13 @@ with open('D:\\notes.txt', 'r', encoding='cp1251') as inf:
             else:
                 note_text = input('Заметка: ')
                 text_notes[note_title] = note_text
-            add_note_text(note_title)
-
+            input_note_text(note_title)
 with open('D:\\notes.txt', 'w') as ouf:
+    count = 0
     for note_title, note_text in text_notes.items():
-        print(note_title + '***' + note_text, file=ouf)
+        count += 1
+        print(str(count) + '***' + note_title + '***' + note_text, file=ouf)
 
-# for key, value in notes.items(): print(key, ':', value)
-# for key in notes.keys(): print(key)
-# for value in notes.values(): print(value)
 # with open(path, 'rb') as f:
 #   contents = f.read()
 # with open('D:\\notes.txt', 'r', encoding='utf-8') as inf:
