c1b864832fa3c93ec14373440284c083fd141be6
Alexandr <apower3@bk.ru>
2021-10-01 12:54:56 +0300

    Done

--- Lesson_003/07_wall.py
+++ Lesson_003/07_wall.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
-
+import simple_draw as sd
+sd.resolution = (1200, 600)
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
+color = sd.COLOR_DARK_YELLOW
+y_0 = 0
+for i in range(12):
+    x_0 = 0
+    if i % 2:
+        x_0 = 50
+    for j in range(12):
+        point_0 = sd.get_point(x_0, y_0)
+        point_1 = sd.get_point(x_0 + 100, y_0 + 50)
+        sd.rectangle(left_bottom=point_0, right_top=point_1, color=color,  width=2)
+        x_0 += 100
+    y_0 += 50
+sd.pause()
 
-simple_draw.pause()
+
