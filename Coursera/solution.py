c1ef333814b8696fd8827791714881fb434067ae
Alexandr <apower.ru@mail.ru>
2022-01-18 00:07:03 +0300

    Work in coursera

--- Coursera/solution.py
+++ Coursera/solution.py
@@ -4,8 +4,7 @@ base = {}
 for line in sys.stdin:
     command, family_summa = line.strip().split(' ', 1)
     if command in ['DEPOSIT', 'WITHDRAW', 'TRANSFER']:
-        if
-        family, summa = family_summa.split()
+        family, summa = family_summa.split(' ', 1)
         base.setdefault(family, 0)
         if command == 'DEPOSIT':
             base[family] += int(summa)
@@ -16,8 +15,9 @@ for line in sys.stdin:
             base.setdefault(family_2, 0)
             base[family] -= int(summa_transfer)
             base[family_2] += int(summa_transfer)
-
-    print(command, family, summa)
-print(base)
-
-
+    elif command == 'BALANCE':
+        print(base.get(family_summa, 'ERROR'))
+    elif command == 'INCOME':
+        for key, value in base.items():
+            if value > 0:
+                value += int(value * (int(family_summa) / 100))
