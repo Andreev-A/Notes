8724ec847ad9909988c4cd891d8f9a019a4e983d
Alexandr <apower3@bk.ru>
2022-02-10 16:59:27 +0300

    Work in application

--- Coursera/Tasks/soup_sample/solution.py
+++ Coursera/Tasks/soup_sample/solution.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-# import unittest
+# # import unittest
 import os
 import re
 
@@ -31,20 +31,153 @@ import re
 start = 'Stone_Age'
 end = 'Python_(programming_language)'
 path = './wiki/'
-files = dict.fromkeys(os.listdir('./wiki/'))  # Словарь вида {"filename1": None, "filename2": None, ...}
-print(files)
-link_list = [start]
-while link_list:
-    for id, link in enumerate(link_list):
-        with open("{}{}".format(path, link), encoding='utf-8') as data:
-            file_links = link_re.findall(data.read())
-            for lnk in [i for i in file_links if i in files.keys()]:
-                if files.get(lnk) is None:
-                    files[lnk] = link
-                    if lnk == end:
-                        print(files)
-                    link_list.append(lnk)
-        link_list.pop(id)
+
+files = os.listdir('./wiki/')
+# print(files)
+out = set()
+with open(os.path.join(path, start), encoding="utf-8") as file:
+    links = re.findall(r"(?<=/wiki/)[\w()]+", file.read())
+    for i in links:
+        if i in files:
+            out.add(i)
+
+# print(links)
+print(sorted(out))
+
+
+
+
+# files = dict.fromkeys(os.listdir('./wiki/'))  # Словарь вида {"filename1": None, "filename2": None, ...}
+# print(files)
+# link_list = [start]
+# while link_list:
+#     for id, link in enumerate(link_list):
+#         with open("{}{}".format(path, link), encoding='utf-8') as data:
+#             file_links = re.findall(r"(?<=/wiki/)[\w()]+", data.read())
+#             for lnk in [i for i in file_links if i in files.keys()]:
+#                 if files.get(lnk) is None:
+#                     files[lnk] = link
+#                     if lnk == end:
+#                         print(files)
+#                     link_list.append(lnk)
+#         link_list.pop(id)
+#
+# from bs4 import BeautifulSoup
+# import re
+# import os
+
+
+# def get_href_page_names(page_name, wiki_path):
+#     try:
+#         with open(os.path.join(wiki_path, page_name), encoding='utf-8') as file:
+#             html = file.read()
+#             soup = BeautifulSoup(html, 'html.parser')
+#             raw_a = soup.find_all('a', href=True)
+#             print(raw_a)
+#             pages_names = [
+#                 x['href'].split('/')[-1]
+#                 for x in raw_a
+#                 if x['href'].startswith('/wiki')
+#             ]
+#             return pages_names
+#     except FileNotFoundError:
+#         return []
+#
+#
+# def bfs_paths(start, goal, wiki_path):
+#     queue = [(start, [start])]
+#     while queue:
+#         (vertex, path) = queue.pop(0)
+#         for next in set(get_href_page_names(vertex, wiki_path)) - set(path):
+#             if next == goal:
+#                 yield path + [next]
+#             else:
+#                 queue.append((next, path + [next]))
+#
+#
+# def shortest_path(start, goal, wiki_path):
+#     try:
+#         return next(bfs_paths(start, goal, wiki_path))
+#     except StopIteration:
+#         return None
+#
+#
+# def get_images_amount(body):
+#     imgs = body.find_all('img')
+#     fit_imgs = len(
+#         [x for x in imgs if x.get('width') and int(x.get('width')) >= 200]
+#     )
+#     return fit_imgs
+#
+#
+# def get_headers_amount(body):
+#     headers = body.find_all(re.compile('^h[1-6]$'))
+#     count = 0
+#     for header in headers:
+#         children = header.find_all(recursive=False)
+#         if children:
+#             children_content = [x.getText() for x in children if x.getText()]
+#             try:
+#                 first_letter = children_content[0][0]
+#                 if first_letter in 'ETC':
+#                     count += 1
+#             except IndexError:
+#                 pass
+#         else:
+#             try:
+#                 first_letter = header.getText()[0]
+#                 if first_letter in 'ETC':
+#                     count += 1
+#             except IndexError:
+#                 pass
+#     return count
+#
+#
+# def get_max_links_len(body):
+#     max_count = 0
+#     all_links = body.find_all('a')
+#     for link in all_links:
+#         current_count = 1
+#         siblings = link.find_next_siblings()
+#         for sibling in siblings:
+#             if sibling.name == 'a':
+#                 current_count += 1
+#                 max_count = max(current_count, max_count)
+#             else:
+#                 current_count = 0
+#     return max_count
+#
+#
+# def get_lists_num(body):
+#     count = 0
+#     all_lists = body.find_all(['ul', 'ol'])
+#     for tag in all_lists:
+#         if not tag.find_parents(['ul', 'ol']):
+#             count += 1
+#     return count
+#
+#
+# def parse(start, end, wiki_path):
+#     bridge = shortest_path(start, end, wiki_path)
+#     out = {}
+#     for file in bridge:
+#         with open(os.path.join(wiki_path, file)) as data:
+#             soup = BeautifulSoup(data, "html.parser")
+#         body = soup.find(id="bodyContent")
+#
+#         imgs = get_images_amount(body)
+#         headers = get_headers_amount(body)
+#         linkslen = get_max_links_len(body)
+#         lists = get_lists_num(body)
+#         out[file] = [imgs, headers, linkslen, lists]
+#     return out
+#
+#
+# result = parse(start, end, path)
+
+
+
+
 
 
 
